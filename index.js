e4811873742c9c3061beac292dff52b87e3ff901
JaehongDev <println@kakao.com>
2022-06-03 15:39:11 +0900

    init

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require("express");
 
 const app = new express();
 
-app.get("/",(req,res,next)=>{
-    res.write("test");
+app.use(express.static(__dirname + "/web"));
 
+
+const createPath = function (path = ""){
+    if( path[path.length-1] === "/") {
+        path += "index";
+    }
+    return path + ".html";
+}
+
+app.get("*",(req,res,next)=>{
+    console.log(createPath(req.path));
+    res.sendFile(createPath(req.path),{root:__dirname+"/static/web"});
 })
+
 
 app.listen(3000,()=>console.log("app start"));
