9260d333d8d3df5c7b01592ffc7c3fb9da565b41
JaehongDev <println@kakao.com>
2022-06-07 11:17:26 +0900

    add login component design

--- js/Login.js
+++ js/Login.js
@@ -1,8 +1,27 @@
 const Login = {
-    init:function(){
+    init: function () {
         const _this = this;
 
+        const loginButton = document.getElementById("loginBtn");
+        //event bubbling close
+        loginButton.addEventListener('click', this.login);
+    },
+
+    login: function (event) {
+        event.preventDefault();
+
+        const $collegeSelector = document.getElementById('collegeSelector');
+        $collegeSelector.value;
+
+        const $id = document.getElementById("id");
+        const $pw = document.getElementById("password");
+
+        const data = {
+            id: $id.value,
+            password: $pw.value,
+            collegeType: $collegeSelector.value
+        };
     }
-
-
 }
+
+Login.init();
